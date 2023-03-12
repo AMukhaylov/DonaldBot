97f4dfc5abb1642e7b460686e9c52747eecb3455
AMukhaylov <arthurcloud@yandex.ru>
2023-03-12 19:39:16 +0500

    Телеграм бот Версия 1.0

--- main.py
+++ main.py
@@ -6,46 +6,59 @@ import requests
 bot = telebot.TeleBot('6052300940:AAHn52m9GZSqg7qRm-FkYpIStJkWGv9VYlA')
 
 
+#   Приветсвенное сообщение и главное меню
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f"Привет, <b>{message.from_user.first_name}!</b>"
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    weather_kgs = types.KeyboardButton('🇰🇬 Погода в Чолпан-Ате')
-    weather_perm = types.KeyboardButton('🇷🇺 Погода в Перми')
+    weather = types.KeyboardButton('🌤 Погода')
     artur_work = types.KeyboardButton('❌ Что делает Артур?')
     course_currency = types.KeyboardButton('💲 Курсы валют')
     random_wow = types.KeyboardButton('🔸 Рандомное число')
     information = types.KeyboardButton('❗️ Информация')
-    markup.add(weather_kgs, weather_perm, artur_work, course_currency, random_wow, information)
+    markup.add(weather, artur_work, course_currency, random_wow, information)
 
-    bot.send_message(message.chat.id, 'Что хочешь узнать? Выбирай:', reply_markup=markup)
+    bot.send_message(message.chat.id, "Что хочешь узнать? Выбирай:", reply_markup=markup)
 
 
+#   API dog.ceo где мы берем случайное фото собаки из блока "Что в коробке?"
 def get_dog():
     response = requests.get('https://dog.ceo/api/breeds/image/random').json()
 
     return response['message']
 
 
+#   Кнопка "Погода"
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
     if message.chat.type == 'private':
-        if message.text == '🇰🇬 Погода в Чолпан-Ате':
+        if message.text == '🌤 Погода':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+            weather_kgs = types.KeyboardButton('🇰🇬 Погода в Чолпан-Ате')
+            weather_perm = types.KeyboardButton('🇷🇺 Погода в Перми')
+            back = types.KeyboardButton('🔙 Назад')
+            markup.add(weather_kgs, weather_perm, back)
+            bot.send_message(message.chat.id, 'Выбери город:', reply_markup=markup)
+
+        elif message.text == '🇰🇬 Погода в Чолпан-Ате':
             bot.send_message(message.chat.id, 'Я пока не умею такого!😢')
 
         elif message.text == '🇷🇺 Погода в Перми':
             bot.send_message(message.chat.id, 'Неее я пока только учусь и незнаю, как это передавать!🧐')
 
+        #   Кнопка "Что делает Артур?"
         elif message.text == '❌ Что делает Артур?':
             photo = open('../../Pictures/Мои изображения/ArthurWork.jpg', "rb")
             bot.send_photo(message.chat.id, photo)
             bot.send_message(message.chat.id, 'Да работает он, всё как обычно!🤪')
 
+        #   Кнопка "Рандомное число?"
         elif message.text == '🔸 Рандомное число':
             bot.send_message(message.chat.id, 'Ваше число: ' + str(random.randint(0, 1000)))
 
+        #   Кнопка "Курсы валют"
         elif message.text == '💲 Курсы валют':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
             course_dollar = types.KeyboardButton('💵 Курс Доллара')
@@ -54,6 +67,7 @@ def bot_message(message):
             markup.add(course_dollar, course_euro, back)
             bot.send_message(message.chat.id, '💲 Курсы валют', reply_markup=markup)
 
+        #   Кнопка "Информация"
         elif message.text == '❗️ Информация':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
             course_dollar = types.KeyboardButton('🤖 О боте')
@@ -62,22 +76,25 @@ def bot_message(message):
             markup.add(course_dollar, course_euro, back)
             bot.send_message(message.chat.id, 'Что интересует?', reply_markup=markup)
 
+        #   Кнопка "Назад"
         elif message.text == '🔙 Назад':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-            weather_kgs = types.KeyboardButton('🇰🇬 Погода в Чолпан-Ате')
-            weather_perm = types.KeyboardButton('🇷🇺 Погода в Перми')
+            weather = types.KeyboardButton('🌤 Погода')
             artur_work = types.KeyboardButton('❌ Что делает Артур?')
             course_currency = types.KeyboardButton('💲 Курсы валют')
             random_wow = types.KeyboardButton('🔸 Рандомное число')
             information = types.KeyboardButton('❗️ Информация')
 
-            markup.add(weather_kgs, weather_perm, artur_work, course_currency, random_wow, information)
+            markup.add(weather, artur_work, course_currency, random_wow, information)
             bot.send_message(message.chat.id, '🔙 Назад', reply_markup=markup)
 
+        #   Кнопка "О боте"
         elif message.text == '🤖 О боте':
-            bot.send_message(message.chat.id, f'Cоздан бот: 11.03.2023')
+            bot.send_message(message.chat.id, f'Cоздан: 11.03.2023')
+            bot.send_message(message.chat.id, f'Версия: 1.0')
             bot.send_message(message.chat.id, f'Автор: Мухайлов А.Р.')
 
+        #   Кнопка "Что в коробке?"
         elif message.text == '📦 Что в коробке?':
             bot.send_photo(message.chat.id, photo=get_dog())
 
